9f6c3d3d96791004bcb507afdd26e131c83a9ed7
Ray Toh <raythx98@gmail.com>
2025-02-10 11:26:05 +0800

    Customize landing page

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -20,7 +20,7 @@ const LandingPage = () => {
   };
 
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center" style={{ marginTop: '15vh' }}>
       <h2 className="my-10 sm:my-16 text-3xl sm:text-6xl lg:text-7xl text-white text-center font-extrabold">
         The only URL Shortener <br /> you&rsquo;ll ever need! 👇
       </h2>
@@ -38,37 +38,54 @@ const LandingPage = () => {
           Shorten!
         </Button>
       </form>
-      <img
-        src="/banner1.jpg" // replace with 2 in small screens
-        className="w-full my-11 md:px-11"
-      />
-      <Accordion type="multiple" collapsible="true" className="w-full md:px-11">
+      <Accordion type="multiple" collapsible="true" className="my-20 w-full md:px-11">
         <AccordionItem value="item-1">
           <AccordionTrigger>
-            How does the Trimrr URL shortener works?
+            Is this URL Shortener free forever?
           </AccordionTrigger>
           <AccordionContent>
-            When you enter a long URL, our system generates a shorter version of
-            that URL. This shortened URL redirects to the original long URL when
-            accessed.
+            <p className="mb-1">It is free, but not forever. </p>
+            <p>We may introduce a donation page or premium plan for advanced features in the future. </p>
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-2">
           <AccordionTrigger>
-            Do I need an account to use the app?
+            Must I create an account to shorten URLs?
           </AccordionTrigger>
           <AccordionContent>
-            Yes. Creating an account allows you to manage your URLs, view
-            analytics, and customize your short URLs.
+            <p className="mb-1">No. You are able to shorten URLs as a guest. </p>
+            <p>However, it is highly recommended to create an account for advanced features. </p>
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-3">
           <AccordionTrigger>
-            What analytics are available for my shortened URLs?
+            <p>What are the benefits of logging in?</p>
           </AccordionTrigger>
           <AccordionContent>
-            You can view the number of clicks, geolocation data of the clicks
-            and device types (mobile/desktop) for each of your shortened URLs.
+            <p className="mb-1">You can manage your URLs in a personalized dashboard. </p>
+            <p>You can also view analytics such as the number of redirects, 
+              and geolocation/device data of the redirects for each of your shortened URLs. </p>
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-4">
+          <AccordionTrigger>
+            Is it safe to use?
+          </AccordionTrigger>
+          <AccordionContent>
+            <p className="mb-1">Rest assured, your passwords are stored securely using <a
+                href="https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html#argon2id"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="nav-link underline"
+              >
+                OWASP recommendations
+              </a>. </p>
+            <p className="mb-1">Please keep a backup of your links.</p>
+            <p>Please report any bugs or security vulnerabilities to me through <a 
+              href="mailto:raythx98@gmail.com" 
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline nav-link">my email</a>.</p>
           </AccordionContent>
         </AccordionItem>
       </Accordion>
